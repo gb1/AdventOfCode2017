25178b5beb3cd3f495375e7680dc3bfada5aa4b6
Gregor Brett <gregor.brett@gmail.com>
2017-12-14 21:25:27 +0000

    day 10 not working

--- lib/day10.ex
+++ lib/day10.ex
@@ -8,16 +8,28 @@ defmodule Aoc.Day10 do
   end
 
   @doc ~S"""
-  iex> Aoc.Day10.knot([0,1,2,3,4], 0, 3, 0)
+  ix> Aoc.Day10.knot([0,1,2,3,4], 0, 3, 0)
   [2,1,0,3,4]
 
-  iex>Aoc.Day10.knot([2,1,0,3,4], 3, 4, 3)
-  []
+  iex>Aoc.Day10.knot([2,1,0,3,4], 3, 4, 1)
+  [4,3,0,1,2]
   """
   def knot(list, position, input_len, skip_size) do
-    {reverse, rest} = Enum.split(list, input_len)
+    
+    #overlap
+    {list1, list2} = Enum.split(list, position)
+    IO.inspect {list1, list2}
+    list = list2 ++ list1
 
-    Enum.reverse(reverse) ++ rest
+    {list1, list2} = Enum.split(list, input_len)
+    IO.inspect {Enum.reverse(list1), list2}
+    list = Enum.reverse(list1) ++ list2
+
+    {list1, list2} = Enum.split(list, skip_size - position)
+    list2 ++ list1
+    # {reverse, rest} = Enum.split(list, input_len)
+
+    # Enum.reverse(reverse) ++ rest
   # knot(list, position + input_len, )
     # Stream.cycle([0,1,2,3,4]) |> Stream.take(5) |> Enum.to_list
   end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -2,26 +2,26 @@ defmodule AocTest do
   use ExUnit.Case
   alias Aoc.Day13
   alias Aoc.Day13.Wall
-  doctest Aoc.Day13
+  doctest Aoc.Day10
 
-  test "scanner goes down" do
-    wall = %Wall{scanner_pos: 1, bricks: 3, direction: :down}
-    moved = %Wall{scanner_pos: 2, bricks: 3, direction: :down}
-    assert Day13.move_wall_scanner(wall) == moved
-  end
+  # test "scanner goes down" do
+  #   wall = %Wall{scanner_pos: 1, bricks: 3, direction: :down}
+  #   moved = %Wall{scanner_pos: 2, bricks: 3, direction: :down}
+  #   assert Day13.move_wall_scanner(wall) == moved
+  # end
 
 
-  test "scanner bounces at the bottom" do
-    wall = %Wall{scanner_pos: 3, bricks: 3, direction: :down}
-    moved = %Wall{scanner_pos: 2, bricks: 3, direction: :up}
-    assert Day13.move_wall_scanner(wall) == moved
-  end
+  # test "scanner bounces at the bottom" do
+  #   wall = %Wall{scanner_pos: 3, bricks: 3, direction: :down}
+  #   moved = %Wall{scanner_pos: 2, bricks: 3, direction: :up}
+  #   assert Day13.move_wall_scanner(wall) == moved
+  # end
 
-  test "scanner bounces at the top" do
-    wall = %Wall{scanner_pos: 1, bricks: 3, direction: :up}
-    moved = %Wall{scanner_pos: 2, bricks: 3, direction: :down}
-    assert Day13.move_wall_scanner(wall) == moved
-  end
+  # test "scanner bounces at the top" do
+  #   wall = %Wall{scanner_pos: 1, bricks: 3, direction: :up}
+  #   moved = %Wall{scanner_pos: 2, bricks: 3, direction: :down}
+  #   assert Day13.move_wall_scanner(wall) == moved
+  # end
 
 
 
